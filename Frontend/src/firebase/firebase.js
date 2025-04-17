fef325a46ae90ce88649535e61aa7f2ec6860ed5
GameDevRocky <159648130+GameDevRocky@users.noreply.github.com>
2025-04-17 13:11:07 -0400

    HIDDEN API KEYS

--- Frontend/src/firebase/firebase.js
+++ Frontend/src/firebase/firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app"; import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
-const firebaseConfig = { apiKey: "AIzaSyDbGAU0jeX8pvdqM5MP7rNlHD7vVoGzEcQ",
-     authDomain: "sceneit-1aa98.firebaseapp.com", 
-     projectId: "sceneit-1aa98", 
-     storageBucket: "sceneit-1aa98.firebasestorage.app", 
-     messagingSenderId: "239652049139",
-      appId: "1:239652049139:web:873db68a8132714427a786", 
-      measurementId: "G-NWHFWWJDTM" }; 
 
 
+const firebaseConfig = {
+     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+     appId: import.meta.env.VITE_FIREBASE_APP_ID,
+     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+   };
       const app = initializeApp(firebaseConfig)
       const auth = getAuth(app)
       export {app, auth};
